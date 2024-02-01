b0e55f609f9f87513388ff0fedd43b9046038be7
arnnvv <arnav.900@protonmail.com>
2024-02-02 03:01:01 +0530

    transactions

--- server/routes/accountRouter.ts
+++ server/routes/accountRouter.ts
@@ -2,6 +2,7 @@ import express from "express";
 import type { Response } from "express";
 import authenticate from "../authenticate.ts";
 import { Accounts } from "../db.ts";
+import mongoose from "mongoose";
 import type { AuthenticatedRequest } from "../authenticate.ts";
 
 const accountRouter = express.Router();
@@ -21,6 +22,43 @@ accountRouter
     } catch (e) {
       console.error(`Error in fetching Balance: ${e}`);
       return res.status(500).json({ message: "Internal Server Error" });
+    }
+  });
+
+accountRouter
+  .use(authenticate)
+  .route("/transfer")
+  .post(async (req: AuthenticatedRequest, res: Response) => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    const { amount, to } = req.body;
+    try {
+      const account = await Accounts.findOne({ userId: req.userId }).session(
+        session,
+      );
+      if (!account || account.balance < amount) {
+        await session.abortTransaction();
+        return res.status(400).json({ message: "Insufficient Balance" });
+      }
+      await Accounts.findOneAndUpdate(
+        { userId: req.userId },
+        {
+          $inc: { balance: -amount },
+        },
+      ).session(session);
+      await Accounts.findOneAndUpdate(
+        { userId: to },
+        {
+          $inc: { balance: amount },
+        },
+      ).session(session);
+      await session.commitTransaction();
+      return res.status(200).json({ message: "Transaction Successful" });
+    } catch (e) {
+      console.error(`Error in transfer: ${e}`);
+      return res.status(500).json({ message: "Internal Server Error" });
+    } finally {
+      session.endSession();
     }
   });
 
