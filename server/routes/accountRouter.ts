e9b41de841fca1411efbc9fee54c6697c33ac006
arnnvv <arnav.900@protonmail.com>
2024-02-02 02:39:42 +0530

    fetchBalance

--- server/routes/accountRouter.ts
+++ server/routes/accountRouter.ts
@@ -1,6 +1,27 @@
 import express from "express";
-import type { Request, Response } from "express";
+import type { Response } from "express";
+import authenticate from "../authenticate.ts";
+import { Accounts } from "../db.ts";
+import type { AuthenticatedRequest } from "../authenticate.ts";
 
 const accountRouter = express.Router();
+
+accountRouter
+  .use(authenticate)
+  .route("/balance")
+  .get(async (req: AuthenticatedRequest, res: Response) => {
+    try {
+      const account = await Accounts.findOne({ userId: req.userId });
+      if (!account)
+        return res.status(404).json({ message: "Account not found" });
+      return res.status(200).json({
+        message: `Balance fetched: ${account.balance}`,
+        balance: account.balance,
+      });
+    } catch (e) {
+      console.error(`Error in fetching Balance: ${e}`);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  });
 
 export default accountRouter;
