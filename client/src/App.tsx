820008018a09afd24c09342559c26ded5a2b6d30
arnnvv <arnav.900@protonmail.com>
2024-02-02 13:23:58 +0530

    components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes></Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
