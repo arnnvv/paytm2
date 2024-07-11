b5210076aaefb5168bd630ef76853cfcd5b906b1
arnnvv <arnav.900@protonmail.com>
2024-07-12 01:28:19 +0530

    updated

--- packages/db/schema.ts
+++ packages/db/schema.ts
@@ -1,6 +1,8 @@
-import { relations } from "drizzle-orm";
+import { Many, One, relations } from "drizzle-orm";
 import {
+  bigint,
   integer,
+  PgColumn,
   pgEnum,
   pgTableCreator,
   timestamp,
@@ -22,9 +24,40 @@ export const onRampStatusEnum = pgEnum("onRampStatusEnum", [
 export const users = createTable("users", {
   id: uuid("id").primaryKey().defaultRandom(),
   name: varchar("name"),
-  email: varchar("email").unique(),
-  number: varchar("number").unique().notNull(),
+  email: varchar("email").unique().notNull(),
+  number: varchar("number").unique(),
   password: varchar("password").notNull(),
+});
+
+export const sessions = createTable("sessions", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  userId: uuid("user_id")
+    .notNull()
+    .references(
+      (): PgColumn<
+        {
+          name: "id";
+          tableName: "users";
+          dataType: "string";
+          columnType: "PgUUID";
+          data: string;
+          driverParam: string;
+          notNull: true;
+          hasDefault: true;
+          isPrimaryKey: true;
+          isAutoincrement: false;
+          hasRuntimeDefault: false;
+          enumValues: undefined;
+          baseColumn: never;
+          generated: undefined;
+        },
+        {},
+        {}
+      > => users.id,
+    ),
+  expires: bigint("expires", {
+    mode: "number",
+  }).notNull(),
 });
 
 export const merchant = createTable("merchant", {
@@ -43,7 +76,28 @@ export const onRampTransaction = createTable("onramp_transaction", {
   startTime: timestamp("start_time").notNull(),
   userId: uuid("user_id")
     .notNull()
-    .references(() => users.id),
+    .references(
+      (): PgColumn<
+        {
+          name: "id";
+          tableName: "users";
+          dataType: "string";
+          columnType: "PgUUID";
+          data: string;
+          driverParam: string;
+          notNull: true;
+          hasDefault: true;
+          isPrimaryKey: true;
+          isAutoincrement: false;
+          hasRuntimeDefault: false;
+          enumValues: undefined;
+          baseColumn: never;
+          generated: undefined;
+        },
+        {},
+        {}
+      > => users.id,
+    ),
 });
 
 export const balance = createTable("balance", {
@@ -51,7 +105,28 @@ export const balance = createTable("balance", {
   userId: uuid("user_id")
     .unique()
     .notNull()
-    .references(() => users.id),
+    .references(
+      (): PgColumn<
+        {
+          name: "id";
+          tableName: "users";
+          dataType: "string";
+          columnType: "PgUUID";
+          data: string;
+          driverParam: string;
+          notNull: true;
+          hasDefault: true;
+          isPrimaryKey: true;
+          isAutoincrement: false;
+          hasRuntimeDefault: false;
+          enumValues: undefined;
+          baseColumn: never;
+          generated: undefined;
+        },
+        {},
+        {}
+      > => users.id,
+    ),
   amount: integer("amount").notNull(),
   locked: integer("locked").notNull(),
 });
@@ -62,22 +137,86 @@ export const p2pTransfer = createTable("p2p_transfer", {
   timestamp: timestamp("timestamp").notNull(),
   fromUserId: uuid("from_user_id")
     .notNull()
-    .references(() => users.id),
+    .references(
+      (): PgColumn<
+        {
+          name: "id";
+          tableName: "users";
+          dataType: "string";
+          columnType: "PgUUID";
+          data: string;
+          driverParam: string;
+          notNull: true;
+          hasDefault: true;
+          isPrimaryKey: true;
+          isAutoincrement: false;
+          hasRuntimeDefault: false;
+          enumValues: undefined;
+          baseColumn: never;
+          generated: undefined;
+        },
+        {},
+        {}
+      > => users.id,
+    ),
   toUserId: uuid("to_user_id")
     .notNull()
-    .references(() => users.id),
+    .references(
+      (): PgColumn<
+        {
+          name: "id";
+          tableName: "users";
+          dataType: "string";
+          columnType: "PgUUID";
+          data: string;
+          driverParam: string;
+          notNull: true;
+          hasDefault: true;
+          isPrimaryKey: true;
+          isAutoincrement: false;
+          hasRuntimeDefault: false;
+          enumValues: undefined;
+          baseColumn: never;
+          generated: undefined;
+        },
+        {},
+        {}
+      > => users.id,
+    ),
 });
 
-export const userRelations = relations(users, ({ many }) => ({
-  onRampTransactions: many(onRampTransaction),
-  balances: many(balance),
-  sentTransfers: many(p2pTransfer, { relationName: "FromUserRelation" }),
-  receivedTransfers: many(p2pTransfer, { relationName: "ToUserRelation" }),
-}));
+export const userRelations = relations(
+  users,
+  ({
+    many,
+  }): {
+    sessions: Many<"sessions">;
+    onRampTransactions: Many<"onramp_transaction">;
+    balances: Many<"balance">;
+    sentTransfers: Many<"p2p_transfer">;
+    receivedTransfers: Many<"p2p_transfer">;
+  } => ({
+    sessions: many(sessions),
+    onRampTransactions: many(onRampTransaction),
+    balances: many(balance),
+    sentTransfers: many(p2pTransfer, { relationName: "FromUserRelation" }),
+    receivedTransfers: many(p2pTransfer, { relationName: "ToUserRelation" }),
+  }),
+);
+
+export const sessionRelations = relations(
+  sessions,
+  ({ one }): { user: One<"users", true> } => ({
+    user: one(users, {
+      fields: [sessions.userId],
+      references: [users.id],
+    }),
+  }),
+);
 
 export const onRampTransactionRelations = relations(
   onRampTransaction,
-  ({ one }) => ({
+  ({ one }): { user: One<"users", true> } => ({
     user: one(users, {
       fields: [onRampTransaction.userId],
       references: [users.id],
@@ -85,22 +224,28 @@ export const onRampTransactionRelations = relations(
   }),
 );
 
-export const balanceRelations = relations(balance, ({ one }) => ({
-  user: one(users, {
-    fields: [balance.userId],
-    references: [users.id],
+export const balanceRelations = relations(
+  balance,
+  ({ one }): { user: One<"users", true> } => ({
+    user: one(users, {
+      fields: [balance.userId],
+      references: [users.id],
+    }),
   }),
-}));
+);
 
-export const p2pTransferRelations = relations(p2pTransfer, ({ one }) => ({
-  fromUser: one(users, {
-    fields: [p2pTransfer.fromUserId],
-    references: [users.id],
-    relationName: "FromUserRelation",
+export const p2pTransferRelations = relations(
+  p2pTransfer,
+  ({ one }): { fromUser: One<"users", true>; toUser: One<"users", true> } => ({
+    fromUser: one(users, {
+      fields: [p2pTransfer.fromUserId],
+      references: [users.id],
+      relationName: "FromUserRelation",
+    }),
+    toUser: one(users, {
+      fields: [p2pTransfer.toUserId],
+      references: [users.id],
+      relationName: "ToUserRelation",
+    }),
   }),
-  toUser: one(users, {
-    fields: [p2pTransfer.toUserId],
-    references: [users.id],
-    relationName: "ToUserRelation",
-  }),
-}));
+);
