db97b98200bb2d0d5766f5e96131f66ce6968d87
arnnvv <arnav.900@protonmail.com>
2024-07-18 17:01:37 +0530

    webhook

--- apps/webhook/src/index.ts
+++ apps/webhook/src/index.ts
@@ -1,5 +1,8 @@
-import { Hono } from "hono";
+import { Context, Hono } from "hono";
 import { cors } from "hono/cors";
+import { db, eq } from "@repo/db/client";
+import { BlankInput } from "hono/types";
+import { balance, onRampTransaction } from "@repo/db/schema";
 
 type Bindings = {
   [key in keyof CloudflareBindings]: CloudflareBindings[key];
@@ -8,13 +11,42 @@ type Bindings = {
 const app = new Hono<{ Bindings: Bindings }>();
 app.use(cors());
 
-app.post("/hdfcWebhook", async (c) => {
-  const { token, user_identifier, amount } = await c.req.json();
-  try {
-  } catch (error) {
-    throw new Error("Error while processing webhook");
-  } finally {
-  }
-});
+app.post(
+  "/hdfcWebhook",
+  async (c: Context<{ Bindings: Bindings }, "/hdfcWebhook", BlankInput>) => {
+    const { token, userId, amount } = await c.req.json();
+    try {
+      const check = await db.query.onRampTransaction.findFirst({
+        where: (onRampTransaction, { eq, and }) =>
+          and(
+            eq(onRampTransaction.token, token),
+            eq(onRampTransaction.userId, userId),
+          ),
+      });
+
+      if (!check) {
+        return c.json({
+          error: "Transaction is already processed",
+          status: 400,
+        });
+      }
+
+      await db.transaction(async (tx): Promise<void> => {
+        await tx
+          .update(balance)
+          .set({ amount: balance.amount + amount })
+          .where(eq(balance.userId, userId));
+        await tx
+          .update(onRampTransaction)
+          .set({ status: "Success" })
+          .where(eq(onRampTransaction.token, token));
+      });
+
+      return c.json({ message: "Payment processed" }, 200);
+    } catch (error) {
+      return c.json({ error: `Something went wrong: ${error}` }, 411);
+    }
+  },
+);
 
 export default app;
--- packages/db/index.ts
+++ packages/db/index.ts
@@ -2,5 +2,6 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 import { getDB } from "./drizzle.config";
+export { eq } from "drizzle-orm";
 
 export const db = drizzle(neon(getDB()), { schema });
